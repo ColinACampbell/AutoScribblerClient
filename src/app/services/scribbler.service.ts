5b5a9f00f5406167fa3f0e07b28e1a26ae0c62e1
Colin Campbell <prontobol@gmail.com>
2021-12-27 17:25:26 -0500

    Can now add content

--- src/app/services/scribbler.service.ts
+++ src/app/services/scribbler.service.ts
@@ -1,14 +1,23 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import TemplateDefinition from '../models/TemplateDefinition.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScribblerService {
 
+  private templateDefinitions = new BehaviorSubject<TemplateDefinition[]>([])
+  public templateDefinitionsObservable = this.templateDefinitions.asObservable();
+
   constructor(private httpClient: HttpClient) { }
 
+  public addTemplateDefinition(templateDefinition:TemplateDefinition)
+  {
+    this.templateDefinitions.value.push(templateDefinition);
+    this.templateDefinitions.next(this.templateDefinitions.value);
+  }
 
   public async submitDocuments(files: File[]): Promise<Observable<HttpResponse<Blob>>> {
     const dataArray: string[] = [];
